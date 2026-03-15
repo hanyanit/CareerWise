79a78354a557bcf1cfb8ccdcea53bd045faa0ba8
Inga Mbobo <mboboinga12@gmail.com>
2026-03-15 21:18:51 +0200

    Skill, Education, JobSeeker classes added in the domain package

---
+++ src/main/java/Domain/Education.java
@@ -0,0 +1,125 @@
+package Domain;
+
+import java.util.Date;
+import java.util.Objects;
+
+public class Education {
+    private String educationId;
+    private String institution;
+    private String degree;
+    private String fieldOfStudy;
+    private Date startDate;
+    private Date endDate;
+    private boolean isCurrent;
+    private String description;
+
+    public Education() {
+    }
+
+    private Education(Builder builder) {
+        this.educationId = builder.educationId;
+        this.institution = builder.institution;
+        this.degree = builder.degree;
+        this.fieldOfStudy = builder.fieldOfStudy;
+        this.startDate = builder.startDate;
+        this.endDate = builder.endDate;
+        this.isCurrent = builder.isCurrent;
+        this.description = builder.description;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        Education education = (Education) o;
+        return isCurrent == education.isCurrent && Objects.equals(educationId, education.educationId) && Objects.equals(institution, education.institution) && Objects.equals(degree, education.degree) && Objects.equals(fieldOfStudy, education.fieldOfStudy) && Objects.equals(startDate, education.startDate) && Objects.equals(endDate, education.endDate) && Objects.equals(description, education.description);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(educationId, institution, degree, fieldOfStudy, startDate, endDate, isCurrent, description);
+    }
+
+    @Override
+    public String toString() {
+        return "Education{" +
+                "educationId='" + educationId + '\'' +
+                ", institution='" + institution + '\'' +
+                ", degree='" + degree + '\'' +
+                ", fieldOfStudy='" + fieldOfStudy + '\'' +
+                ", startDate=" + startDate +
+                ", endDate=" + endDate +
+                ", isCurrent=" + isCurrent +
+                ", description='" + description + '\'' +
+                '}';
+    }
+
+
+    private static  class Builder {
+        private String educationId;
+        private String institution;
+        private String degree;
+        private String fieldOfStudy;
+        private Date startDate;
+        private Date endDate;
+        private boolean isCurrent;
+        private String description;
+
+
+
+        public Builder setEducationId(String educationId) {
+            this.educationId = educationId;
+            return this;
+        }
+
+        public Builder setInstitution(String institution) {
+            this.institution = institution;
+            return this;
+        }
+
+        public Builder setDegree(String degree) {
+            this.degree = degree;
+            return this;
+        }
+
+        public Builder setFieldOfStudy(String fieldOfStudy) {
+            this.fieldOfStudy = fieldOfStudy;
+            return this;
+        }
+
+        public Builder setStartDate(Date startDate) {
+            this.startDate = startDate;
+            return this;
+        }
+
+        public Builder setEndDate(Date endDate) {
+            this.endDate = endDate;
+            return this;
+        }
+
+        public Builder setCurrent(boolean current) {
+            isCurrent = current;
+            return this;
+        }
+
+        public Builder setDescription(String description) {
+            this.description = description;
+            return this;
+        }
+
+        public Builder copy() {
+            return new Builder()
+                    .setEducationId(this.educationId)
+                    .setInstitution(this.institution)
+                    .setDegree(this.degree)
+                    .setFieldOfStudy(this.fieldOfStudy)
+                    .setStartDate(this.startDate)
+                    .setEndDate(this.endDate)
+//                    .setIsCurrent(this.isCurrent)
+                    .setDescription(this.description);
+        }
+
+        public  Education build() {
+            return new Education(this);
+        }
+    }
+}
---
+++ src/main/java/Domain/JobSeeker.java
@@ -0,0 +1,21 @@
+package Domain;
+
+import Domain.Education;
+import Domain.Skill;
+import java.io.File;
+import java.util.List;
+
+public class JobSeeker {
+    private String headline;
+    private String summary;
+    private File resume;
+    private List<Skill> skills;
+    private List<Education> education;
+//    private List<Experience> experience;
+//    private List<Certification> certifications;
+//    private List<Language> languages;
+//    private JobPreferences jobPreferences;
+
+//    I will finish this class once the jobSeeker subclasses are done
+
+}
