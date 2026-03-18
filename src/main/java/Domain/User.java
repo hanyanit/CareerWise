4e1df52de8780f84cb0df686f207dbcbd900577a
Hanyani Masinge <hanyanimasinge@Hanyanis-MacBook-Air.local>
2026-03-18 19:58:44 +0200

    builder class(user)

--- src/main/java/Domain/User.java
+++ src/main/java/Domain/User.java
@@ -2,5 +2,71 @@ package Domain;
 
 public class User {
 
-    int ID;
+    private String userId;
+    private String email;
+    private String password;
+    private String firstName;
+    private String lastName;
+    private String profilePicture;
+    private String phoneNumber;
+    private String location;
+
+
+    private User(Builder builder) {
+        this.userId = builder.userId;
+        this.email = builder.email;
+        this.password = builder.password;
+        this.firstName = builder.firstName;
+        this.lastName = builder.lastName;
+        this.profilePicture = builder.profilePicture;
+        this.phoneNumber = builder.phoneNumber;
+        this.location = builder.location;
+    }
+
+
+    public static class Builder {
+        private String userId;
+        private String email;
+        private String password;
+        private String firstName;
+        private String lastName;
+        private String profilePicture;
+        private String phoneNumber;
+        private String location;
+
+        public Builder(String userId, String email, String password) {
+            this.userId = userId;
+            this.email = email;
+            this.password = password;
+        }
+
+        public Builder firstName(String firstName) {
+            this.firstName = firstName;
+            return this;
+        }
+
+        public Builder lastName(String lastName) {
+            this.lastName = lastName;
+            return this;
+        }
+
+        public Builder profilePicture(String profilePicture) {
+            this.profilePicture = profilePicture;
+            return this;
+        }
+
+        public Builder phoneNumber(String phoneNumber) {
+            this.phoneNumber = phoneNumber;
+            return this;
+        }
+
+        public Builder location(String location) {
+            this.location = location;
+            return this;
+        }
+
+        public User build() {
+            return new User(this);
+        }
+    }
 }
