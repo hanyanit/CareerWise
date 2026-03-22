0c80ff63d21dfc9d79d2dc8c041ab6bac60a05a1
Inga Mbobo <mboboinga12@gmail.com>
2026-03-22 23:28:30 +0200

    Experience,JobSeeker and Skill repositories

--- src/main/java/za/ac/cput/repository/JobSeekerRepository.java
+++ src/main/java/za/ac/cput/repository/JobSeekerRepository.java
@@ -4,18 +4,21 @@ import za.ac.cput.domain.JobSeeker;
 
 import java.util.ArrayList;
 import java.util.List;
-
-public class JobSeekerRepository implements IJobSeekerRepository{
+/**
+ * Author: Inga Mbobo, 230711723
+ * Date: March 2026
+ */
+public class JobSeekerRepository implements IJobSeekerRepository {
 
     private static IJobSeekerRepository repository = null;
     private List<JobSeeker> jobSeekerList;
 
-    private JobSeekerRepository(){
+    private JobSeekerRepository() {
         jobSeekerList = new ArrayList<>();
     }
 
-    private static IJobSeekerRepository getRepository(){
-        if(repository == null){
+    public static IJobSeekerRepository getRepository() {
+        if (repository == null) {
             repository = new JobSeekerRepository();
         }
         return repository;
@@ -23,26 +26,53 @@ public class JobSeekerRepository implements IJobSeekerRepository{
 
     @Override
     public JobSeeker create(JobSeeker jobSeeker) {
+        boolean success = jobSeekerList.add(jobSeeker);
+        if (success) {
+            return jobSeeker;
+        }
         return null;
     }
 
     @Override
-    public JobSeeker read(String s) {
-        return null;
+    public JobSeeker read(String userId) { // I will code this method using lambda expression
+        return jobSeekerList.stream()
+                .filter(jobSeeker -> jobSeeker.getUserId().equals(userId))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public JobSeeker update(JobSeeker jobSeeker) {
+        String id = jobSeeker.getUserId();
+        JobSeeker oldJobSeeker = read(id);
+
+        if (oldJobSeeker == null) {
+            return null;
+        }
+
+        boolean success = jobSeekerList.remove(oldJobSeeker);
+        if (!success) {
+            return null;
+        }
+
+        if (jobSeekerList.add(jobSeeker)) {
+            return jobSeeker;
+        }
+
         return null;
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String userId) {
+        JobSeeker jobSeekerToDelete = read(userId);
+        if (jobSeekerToDelete == null) {
+            return false;
+        }
+        return jobSeekerList.remove(jobSeekerToDelete);
     }
 
     @Override
     public List<JobSeeker> getAll() {
-        return List.of();
+        return jobSeekerList;
     }
 }
--- src/main/java/za/ac/cput/repository/SkillRepository.java
+++ src/main/java/za/ac/cput/repository/SkillRepository.java
@@ -8,18 +8,17 @@ import java.util.List;
  * Author: Inga Mbobo, 230711723
  * Date: March 2026
  */
-public class SkillRepository implements ISkillRepository{
+public class SkillRepository implements ISkillRepository {
 
     private static ISkillRepository repository = null;
     private List<Skill> skillsList;
 
-    //singleton design pattern
-    private SkillRepository(){
+    private SkillRepository() {
         skillsList = new ArrayList<>();
     }
 
-    private static ISkillRepository getRepository(){
-        if(repository == null){
+    public static ISkillRepository getRepository() {
+        if (repository == null) {
             repository = new SkillRepository();
         }
         return repository;
@@ -27,26 +26,53 @@ public class SkillRepository implements ISkillRepository{
 
     @Override
     public Skill create(Skill skill) {
+        boolean success = skillsList.add(skill);
+        if (success) {
+            return skill;
+        }
         return null;
     }
 
     @Override
-    public Skill read(String s) {
-        return null;
+    public Skill read(String skillId) {
+        return skillsList.stream()
+                .filter(skill -> skill.getSkillId().equals(skillId))
+                .findFirst()
+                .orElse(null);
     }
 
     @Override
     public Skill update(Skill skill) {
+        String id = skill.getSkillId();
+        Skill oldSkill = read(id);
+
+        if (oldSkill == null) {
+            return null;
+        }
+
+        boolean success = skillsList.remove(oldSkill);
+        if (!success) {
+            return null;
+        }
+
+        if (skillsList.add(skill)) {
+            return skill;
+        }
+
         return null;
     }
 
     @Override
-    public boolean delete(String s) {
-        return false;
+    public boolean delete(String skillId) {
+        Skill skillToDelete = read(skillId);
+        if (skillToDelete == null) {
+            return false;
+        }
+        return skillsList.remove(skillToDelete);
     }
 
     @Override
     public List<Skill> getAll() {
-        return List.of();
+        return skillsList;
     }
 }
