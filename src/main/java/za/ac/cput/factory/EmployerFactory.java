14db278eb92cb8dc5c57b7b312f3a5a2445f7737
masin <221568816@mycput.ac.za>
2026-03-24 13:19:40 +0200

    EmployerRepository IEmployerRepository EmployerFactoryTest

--- src/main/java/za/ac/cput/factory/EmployerFactory.java
+++ src/main/java/za/ac/cput/factory/EmployerFactory.java
@@ -1,15 +1,18 @@
 package za.ac.cput.factory;
 
 import za.ac.cput.domain.Employer;
+import za.ac.cput.util.Helper;
 
 /**
+ * EmployerFactory.java
  * Author: Andile Pamela Masina, 221568816
- * Date: 17 March 2026
+ * Date: 24 March 2026
  */
 public class EmployerFactory {
 
     public static Employer buildEmployer(String companyName, String industry, String companySize, String companyHeadquarters) {
-        if (companyName == null || companyName.isEmpty() || industry == null || industry.isEmpty()) {
+
+        if (Helper.isNullOrEmpty(companyName) || Helper.isNullOrEmpty(industry)) {
             return null;
         }
 
