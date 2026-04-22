d7b1ad3ea158b13ebcf6b8c9f5a0da20c1e7a9b5
Hanyani Masinge <hanyanimasinge@Hanyanis-MacBook-Air.local>
2026-04-22 11:19:41 +0200

    service and controller package

--- src/main/java/za/ac/cput/domain/User.java
+++ src/main/java/za/ac/cput/domain/User.java
@@ -1,9 +1,9 @@
 
 package za.ac.cput.domain;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
 
 /**
  * User.java
@@ -12,7 +12,8 @@ import javax.persistence.Id;
 @Entity
 public class User {
 
-   @Id @GeneratedValue
+   @Id
+   @GeneratedValue
     private int id;
 
     private String userId;
